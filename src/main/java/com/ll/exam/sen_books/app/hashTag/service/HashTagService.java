c6d288534b3b62102e0f56de25f5e4880392c910
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-19 15:48:00 +0900

    해시태그 저장기능 구현

---
+++ src/main/java/com/ll/exam/sen_books/app/hashTag/service/HashTagService.java
@@ -0,0 +1,51 @@
+package com.ll.exam.sen_books.app.hashTag.service;
+
+import com.ll.exam.sen_books.app.hashTag.entity.HashTag;
+import com.ll.exam.sen_books.app.hashTag.repository.HashTagRepository;
+import com.ll.exam.sen_books.app.keyword.entity.Keyword;
+import com.ll.exam.sen_books.app.keyword.service.KeywordService;
+import com.ll.exam.sen_books.app.post.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class HashTagService {
+    private final KeywordService keywordService;
+    private final HashTagRepository hashTagRepository;
+
+    public void applyHashTags(Post post, String keywordContentsStr) {
+        List<String> keywordContents = Arrays.stream(keywordContentsStr.split("#"))
+                .map(String::trim)
+                .filter(s -> s.length() > 0)
+                .collect(Collectors.toList());
+
+        keywordContents.forEach(keywordContent -> {
+            saveHashTag(post, keywordContent);
+        });
+    }
+
+    private HashTag saveHashTag(Post post, String keywordContent) {
+        Keyword keyword = keywordService.save(keywordContent);
+
+        Optional<HashTag> opHashTag = hashTagRepository.findByPostIdAndKeywordId(post.getId(), keyword.getId());
+
+        if (opHashTag.isPresent()) {
+            return opHashTag.get();
+        }
+
+        HashTag hashTag = HashTag.builder()
+                .post(post)
+                .keyword(keyword)
+                .build();
+
+        hashTagRepository.save(hashTag);
+
+        return hashTag;
+    }
+}
