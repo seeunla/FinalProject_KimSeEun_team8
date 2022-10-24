3c31cd94544ea495d3c22274bae928caf99746ed
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-24 10:48:47 +0900

    2주차 디렉토리 생성

---
+++ 2Week_Misson/src/main/java/com/ll/exam/sen_books/app/member/service/CustomMemberService.java
@@ -0,0 +1,35 @@
+package com.ll.exam.sen_books.app.member.service;
+
+import com.ll.exam.sen_books.app.member.entity.Member;
+import com.ll.exam.sen_books.app.member.repository.MemberRepository;
+import com.ll.exam.sen_books.app.security.dto.MemberContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomMemberService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if ( member.getAuthLevel() > 6) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+        authorities.add(new SimpleGrantedAuthority("MEMBER"));
+
+        return new MemberContext(member, authorities);
+    }
+}
