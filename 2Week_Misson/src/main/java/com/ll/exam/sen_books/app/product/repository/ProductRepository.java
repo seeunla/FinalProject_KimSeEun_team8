6079fa08754ff5b2c1b7f592dbd3f52bbb3edbd0
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-25 19:53:51 +0900

    작업

--- 2Week_Misson/src/main/java/com/ll/exam/sen_books/app/product/repository/ProductRepository.java
+++ 2Week_Misson/src/main/java/com/ll/exam/sen_books/app/product/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.ll.exam.sen_books.app.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByAuthorId(Long id);
+    Optional<Product> findById(Long id);
 }
