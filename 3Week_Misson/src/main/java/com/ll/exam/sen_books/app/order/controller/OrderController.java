9769b929eaae0c2ea909c6a80e0bc7774457e975
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-02 01:13:46 +0900

    토스로 결제 구현

--- 3Week_Misson/src/main/java/com/ll/exam/sen_books/app/order/controller/OrderController.java
+++ 3Week_Misson/src/main/java/com/ll/exam/sen_books/app/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sen_books.app.order.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.sen_books.app.cart.entity.CartItem;
 import com.ll.exam.sen_books.app.member.entity.Member;
@@ -9,6 +10,13 @@ import com.ll.exam.sen_books.app.order.service.OrderService;
 import com.ll.exam.sen_books.app.security.dto.MemberContext;
 import com.ll.exam.sen_books.util.Ut;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -17,9 +25,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -88,6 +102,84 @@ public class OrderController {
         orderService.payByRestCashOnly(order);
 
         return "redirect:/order/%d?msg=".formatted(order.getId()) + Ut.url.encode(("%d번 품목을 예치금으로 결제하였습니다.").formatted(id));
+    }
+
+    @PostConstruct
+    private void init() {
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+            }
+        });
+    }
+
+    @Value("${custom.tossPayments.secretKey}")
+    private String SECRET_KEY;
+
+    @RequestMapping("/{id}/success")
+    public String confirmPayment(
+            @PathVariable long id,
+            @RequestParam String paymentKey,
+            @RequestParam String orderId,
+            @RequestParam Long amount,
+            Model model,
+            @AuthenticationPrincipal MemberContext memberContext
+    ) throws Exception {
+
+        Order order = orderService.findForPrintById(id).get();
+
+        long orderIdInputed = Long.parseLong(orderId.split("__")[1]);
+
+        if (id != orderIdInputed) {
+            return "redirect:/order/fail?msg=" + Ut.url.encode( "기존 주문과 일치하지 않습니다.");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
+        headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> payloadMap = new HashMap<>();
+        payloadMap.put("orderId", orderId);
+        payloadMap.put("amount", String.valueOf(amount));
+
+        Member actor = memberContext.getMember();
+        long restCash = memberService.getRestCash(actor);
+        long payPriceRestCash = order.calculatePayPrice() - amount;
+
+        if (payPriceRestCash > restCash) {
+            return "redirect:/order/fail?msg=" + Ut.url.encode( "예치금이 부족합니다.");
+        }
+
+        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
+                "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+
+            orderService.payByTossPayments(order, payPriceRestCash);
+
+            return "redirect:/order/%d".formatted(order.getId()) + Ut.url.encode(
+                    "%d번 주문이 결제처리되었습니다.".formatted(order.getId()));
+        } else {
+            JsonNode failNode = responseEntity.getBody();
+            model.addAttribute("message", failNode.get("message").asText());
+            model.addAttribute("code", failNode.get("code").asText());
+            return "order/fail";
+        }
+    }
+
+    @RequestMapping("/{id}/fail")
+    public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("code", code);
+        return "order/fail";
     }
 
     @PostMapping("/{orderId}/cancel")
