ee43014f5b68130345eca998322cf7b4e87fa5e8
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-02 02:23:37 +0900

    관리자 페이지(구현중)

---
+++ 3Week_Misson/src/main/java/com/ll/exam/sen_books/app/home/controller/AdmHomeController.java
@@ -0,0 +1,22 @@
+package com.ll.exam.sen_books.app.home.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/adm")
+public class AdmHomeController {
+    @GetMapping("")
+    public String showIndex() {
+        return "redirect:/adm/home/main";
+    }
+
+    @GetMapping("/home/main")
+    public String showMain() {
+        return "adm/home/main";
+    }
+
+}
