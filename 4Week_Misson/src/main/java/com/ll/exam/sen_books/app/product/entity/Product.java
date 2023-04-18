959e04e2d1f602d6bf1b9fc6fcba504ae615823e
seeunla <107592967+seeunla@users.noreply.github.com>
2023-04-18 15:11:16 +0900

    Refactor: 상품 디테일에 설명과 작가이름 추가

--- 4Week_Misson/src/main/java/com/ll/exam/sen_books/app/product/entity/Product.java
+++ 4Week_Misson/src/main/java/com/ll/exam/sen_books/app/product/entity/Product.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sen_books.app.product.entity;
 
 import com.ll.exam.sen_books.app.base.entity.BaseEntity;
+import com.ll.exam.sen_books.app.hashTag.entity.ProductHashTag;
 import com.ll.exam.sen_books.app.postKeyword.entity.PostKeyword;
 import com.ll.exam.sen_books.app.member.entity.Member;
 import com.ll.exam.sen_books.app.post.entity.Post;
@@ -10,9 +11,14 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
@@ -32,6 +38,9 @@ public class Product extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String content; // 상품설명
     private int price; // 판매가
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.ALL})
+    private List<ProductHashTag> productHashTags = new ArrayList<>();   // 도서 해시태그 리스트
 
     public Product(long id) {
         super(id);
