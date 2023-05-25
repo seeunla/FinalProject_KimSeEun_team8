2abb36ea47d4aadb209a5b9aa0eb44d0194e0506
seeunla <107592967+seeunla@users.noreply.github.com>
2023-05-25 23:13:17 +0900

    Feat: 현재 날짜에 해당 년/월의 정산 데이터를 생성할 수 있는지 검증 로직 추가

--- 4Week_Misson/src/main/java/com/ll/exam/sen_books/app/rebate/service/RebateService.java
+++ 4Week_Misson/src/main/java/com/ll/exam/sen_books/app/rebate/service/RebateService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,21 +27,38 @@ public class RebateService {
     public void makeDate(int year, int month) {
         int monthEndDay = Ut.date.getEndDay(year, month);
 
-        // 1. 정산 데이터를 생성할 날짜 범위 구하기
-        LocalDateTime fromDate = Ut.date.getStartOfDay(year, month, 1);
-        LocalDateTime toDate = Ut.date.getEndOfDay(year, month, monthEndDay);
+        if (canMakeData(year, month)) {
+            // 1. 정산 데이터를 생성할 날짜 범위 구하기
+            LocalDateTime fromDate = Ut.date.getStartOfDay(year, month, 1);
+            LocalDateTime toDate = Ut.date.getEndOfDay(year, month, monthEndDay);
 
-        // 2. 해당 범위의 모든 주문 품목 조회
-        List<OrderItem> orderItems = orderService.findAllByPayDateBetweenOrderByIdAsc(fromDate, toDate);
 
-        // 3. 주문 데이터 -> 정산 데이터 변환
-        List<RebateOrderItem> rebateOrderItems = orderItems
-                .stream()
-                .map(this::toRebateOrderItem)
-                .collect(Collectors.toList());
+            // 2. 해당 범위의 모든 주문 품목 조회
+            List<OrderItem> orderItems = orderService.findAllByPayDateBetweenOrderByIdAsc(fromDate, toDate);
 
-        // 4. 정산 데이터 생성
-        rebateOrderItems.forEach(this::makeRebateOrderItem);
+            // 3. 주문 데이터 -> 정산 데이터 변환
+            List<RebateOrderItem> rebateOrderItems = orderItems
+                    .stream()
+                    .map(this::toRebateOrderItem)
+                    .collect(Collectors.toList());
+
+            // 4. 정산 데이터 생성
+            rebateOrderItems.forEach(this::makeRebateOrderItem);
+        }
+    }
+
+    // 정산 데이터 생성 가능한지 검증
+    private boolean canMakeData(int year, int month) {
+        LocalDateTime dataCreationDate = LocalDateTime.of(year, month, 15, 4, 0)
+                .plusMonths(1);     // 정산 데이터 예상 생성일시
+        // 현재 날짜 기준 해당 월의 정산 데이터를 생성가능한지 검증
+        if(LocalDateTime.now().isBefore(dataCreationDate)) {
+            throw new RuntimeException("%d-%2d 의 정산 데이터 생성은 %s 이후에 가능합니다.".formatted(
+                    year, month,
+                    dataCreationDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"))
+            ));
+        }
+        return true;
     }
 
     @Transactional
