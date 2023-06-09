a25fae660f8f58fd26b69ab422dfce915ea79d2a
seeunla <107592967+seeunla@users.noreply.github.com>
2023-06-09 23:34:29 +0900

    Feat: @EnableScheduling 추가, @Scheduled로 매 분 00초마다 메서드 실행 테스트

---
+++ 4Week_Misson/src/main/java/com/ll/exam/sen_books/job/makeRabateOrderItem/makeRebateOrderItemJobConfig.java
@@ -0,0 +1,15 @@
+package com.ll.exam.sen_books.job.makeRabateOrderItem;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class makeRebateOrderItemJobConfig {
+    // 매 분 00초마다 실행
+    @Scheduled(cron = "0 * * * * *")
+    public void printJob() {
+        System.out.println(LocalDateTime.now());
+    }
+}
